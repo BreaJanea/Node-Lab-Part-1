1de9e6d0476748787698a3b59ff6763d1dfbdf24
BreaJanea <breajanea@gmail.com>
2019-05-20 18:22:17 -0400

    first commit

---
+++ quotes.js
@@ -0,0 +1,12 @@
+const quotes = [
+    
+    "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.-Maya Angelou",
+    "Nothing will work unless you do. -Maya Angelou",
+    "If you don't like something, change it. If you can't change it, change your attitude.-Maya Angelou",
+    "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.-Maya Angelou",
+    "When you learn, teach. When you get, give.-Maya Angelou"
+]
+
+module.exports = { 
+    quotes
+ };
---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+const express = require("express");
+const app = express();
+
+const { quotes } =  require ('./quotes');
+
+app.get('/', (req,res) => {
+res.send(quotes[Math.floor(Math.random() * quotes.length)]);
+});
+    
+app.listen(3000, () => console.log('Serve up and running on port: 3000'));
